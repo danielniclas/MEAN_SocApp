874f253eea1d62c3c8ce8af94f4e1f8292748f2e
Daniel Niclas <dniclas@sbcglobal.net>
2016-04-06 22:28:03 -0700

    Comments and Clarification

--- app/server.js
+++ app/server.js
@@ -14,12 +14,16 @@ app.use(bodyParser.json());
 
 // app.use(require('./controllers/api/posts'));             //  This file using the Express Router OBJECT  --  Get/Post Endpoints
 
+//  pg 61 --  MIDDLEWARE  app.use()
 //  REQUIRE:  ENDPOINTS  --  These files EXPORT the Router OBJECT
 
-app.use('/api/posts', require('./controllers/api/posts'));  //  .use(1,2)  1.  namespace the routes (ENDPOINT) 2.  Required file
-app.use('/', require('./controllers/static'));              //  Shell Page:  _posts.html
+//  Parameter 1:  API PATHS!!!!  Entered in as URLs and USED by ANGULAR
+
+app.use('/api/posts', require('./controllers/api/posts'));  //  CONTROLLERS:  POSTS >> .use(1,2)  1.  NAMESPACE the routes (ENDPOINT) 2.  Required file
+app.use('/', require('./controllers/static'));              //  CONTROLLERS:  STATIC CONTENT:  Shell Page:  posts.html, app.css, app.js
+
 
 
 app.listen(3000, function() {
-    console.log('The Node server is listening on 3000 -> server.js Node App')
+    console.log('The Node server is listening on 3000 -> server.js Node App  >> SERVER.JS')
 });
